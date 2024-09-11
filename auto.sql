00b0f6f0cd02674e48a5f0e9c483e51218e15cd3
Maksim Dotskin <124581027+DotskinMaksim@users.noreply.github.com>
2024-09-11 11:32:45 +0300

    sql laused tabeli auto loomiseks

---
+++ auto.sql
@@ -0,0 +1,117 @@
+create database auto_tar;
+use auto_tar;
+
+
+create table auto (
+	registri_number VARCHAR(50),
+	mark VARCHAR(50),
+	omanik VARCHAR(50),
+	vaasta VARCHAR(50)
+);
+insert into auto (registri_number, mark, omanik, vaasta) values ('182HLL', 'Audi', 'Eire', 2011);
+insert into auto (registri_number, mark, omanik, vaasta) values ('484RWO', 'Ford', 'Lazz', 2012);
+insert into auto (registri_number, mark, omanik, vaasta) values ('473BQJ', 'Mercury', 'Brainsphere', 1969);
+insert into auto (registri_number, mark, omanik, vaasta) values ('671MAO', 'Mercedes-Benz', 'Wikido', 2011);
+insert into auto (registri_number, mark, omanik, vaasta) values ('390YYC', 'Chevrolet', 'Oyondu', 1992);
+insert into auto (registri_number, mark, omanik, vaasta) values ('890SNE', 'Saab', 'Oozz', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('319EQK', 'GMC', 'Geba', 2011);
+insert into auto (registri_number, mark, omanik, vaasta) values ('078VEW', 'Ford', 'Jayo', 2006);
+insert into auto (registri_number, mark, omanik, vaasta) values ('946RAD', 'Audi', 'Twimbo', 1991);
+insert into auto (registri_number, mark, omanik, vaasta) values ('244ZNU', 'GMC', 'Jetwire', 2006);
+insert into auto (registri_number, mark, omanik, vaasta) values ('354PIT', 'GMC', 'Realbridge', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('259HZY', 'Ford', 'Mudo', 2006);
+insert into auto (registri_number, mark, omanik, vaasta) values ('397PAT', 'Toyota', 'Fatz', 1997);
+insert into auto (registri_number, mark, omanik, vaasta) values ('678HPE', 'Honda', 'Tagchat', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('221NTC', 'Ford', 'Yozio', 1996);
+insert into auto (registri_number, mark, omanik, vaasta) values ('274FHL', 'Pontiac', 'Eayo', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('690HPQ', 'Oldsmobile', 'Devshare', 1993);
+insert into auto (registri_number, mark, omanik, vaasta) values ('198RVU', 'Pontiac', 'Rhybox', 1993);
+insert into auto (registri_number, mark, omanik, vaasta) values ('417RMT', 'Chevrolet', 'Katz', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('444SEA', 'GMC', 'Twiyo', 1998);
+insert into auto (registri_number, mark, omanik, vaasta) values ('342MXO', 'Chevrolet', 'Yakitri', 1999);
+insert into auto (registri_number, mark, omanik, vaasta) values ('043CZQ', 'Mitsubishi', 'Tambee', 1989);
+insert into auto (registri_number, mark, omanik, vaasta) values ('076SIO', 'Lotus', 'Tagtune', 1987);
+insert into auto (registri_number, mark, omanik, vaasta) values ('379LXQ', 'Nissan', 'Feedfire', 2005);
+insert into auto (registri_number, mark, omanik, vaasta) values ('881YVR', 'Volkswagen', 'Avavee', 1999);
+insert into auto (registri_number, mark, omanik, vaasta) values ('043XEE', 'Porsche', 'Eamia', 1997);
+insert into auto (registri_number, mark, omanik, vaasta) values ('815UTJ', 'Mercedes-Benz', 'Youspan', 2011);
+insert into auto (registri_number, mark, omanik, vaasta) values ('233HKQ', 'Suzuki', 'Avamba', 2012);
+insert into auto (registri_number, mark, omanik, vaasta) values ('596PIV', 'Lexus', 'Babbleblab', 1990);
+insert into auto (registri_number, mark, omanik, vaasta) values ('777IMV', 'Toyota', 'Quatz', 1995);
+insert into auto (registri_number, mark, omanik, vaasta) values ('497XFM', 'Mercedes-Benz', 'Oyoyo', 1997);
+insert into auto (registri_number, mark, omanik, vaasta) values ('225DYX', 'Mercedes-Benz', 'Trilith', 2002);
+insert into auto (registri_number, mark, omanik, vaasta) values ('863JCL', 'Mazda', 'Thoughtbeat', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('109WZR', 'GMC', 'Kwideo', 1996);
+insert into auto (registri_number, mark, omanik, vaasta) values ('349NZU', 'Cadillac', 'Einti', 1995);
+insert into auto (registri_number, mark, omanik, vaasta) values ('740KUM', 'Mazda', 'Youspan', 2003);
+insert into auto (registri_number, mark, omanik, vaasta) values ('726HBE', 'Jaguar', 'Ailane', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('430MUA', 'Ford', 'Omba', 1994);
+insert into auto (registri_number, mark, omanik, vaasta) values ('437IKS', 'Mercedes-Benz', 'Bluejam', 1987);
+insert into auto (registri_number, mark, omanik, vaasta) values ('629PTW', 'Ford', 'Kwimbee', 1993);
+insert into auto (registri_number, mark, omanik, vaasta) values ('748JXF', 'Dodge', 'Zoomdog', 1994);
+insert into auto (registri_number, mark, omanik, vaasta) values ('104HZL', 'Mercedes-Benz', 'Realmix', 2011);
+insert into auto (registri_number, mark, omanik, vaasta) values ('457EQA', 'Buick', 'Realmix', 1992);
+insert into auto (registri_number, mark, omanik, vaasta) values ('809RMS', 'Volvo', 'Talane', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('563QDV', 'Dodge', 'Zoonder', 1995);
+insert into auto (registri_number, mark, omanik, vaasta) values ('267PER', 'Hummer', 'Janyx', 1999);
+insert into auto (registri_number, mark, omanik, vaasta) values ('921KTK', 'Mazda', 'Centidel', 2002);
+insert into auto (registri_number, mark, omanik, vaasta) values ('150PAN', 'Scion', 'Wordware', 2009);
+insert into auto (registri_number, mark, omanik, vaasta) values ('840TZY', 'Dodge', 'Babbleset', 1999);
+insert into auto (registri_number, mark, omanik, vaasta) values ('337CEW', 'Ford', 'Voomm', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('240XUH', 'Maserati', 'Zazio', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('896EQE', 'BMW', 'Yata', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('611FNR', 'Lincoln', 'Katz', 2004);
+insert into auto (registri_number, mark, omanik, vaasta) values ('676YIT', 'Jeep', 'Brightdog', 2006);
+insert into auto (registri_number, mark, omanik, vaasta) values ('528IDP', 'BMW', 'Skippad', 2003);
+insert into auto (registri_number, mark, omanik, vaasta) values ('937CMD', 'GMC', 'Skyble', 1993);
+insert into auto (registri_number, mark, omanik, vaasta) values ('616QIU', 'Porsche', 'Katz', 2003);
+insert into auto (registri_number, mark, omanik, vaasta) values ('216BXG', 'Jeep', 'Jabbersphere', 2009);
+insert into auto (registri_number, mark, omanik, vaasta) values ('340BOU', 'Mitsubishi', 'Eabox', 1998);
+insert into auto (registri_number, mark, omanik, vaasta) values ('061HIX', 'Dodge', 'Meezzy', 2009);
+insert into auto (registri_number, mark, omanik, vaasta) values ('911SHV', 'Cadillac', 'Riffwire', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('909FNS', 'Toyota', 'Thoughtstorm', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('493XAU', 'Nissan', 'Ozu', 2010);
+insert into auto (registri_number, mark, omanik, vaasta) values ('935JRD', 'Kia', 'Yotz', 1999);
+insert into auto (registri_number, mark, omanik, vaasta) values ('778LKR', 'Dodge', 'Yabox', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('120KSB', 'Mercury', 'Zooveo', 1992);
+insert into auto (registri_number, mark, omanik, vaasta) values ('529NKO', 'Land Rover', 'Riffpath', 2005);
+insert into auto (registri_number, mark, omanik, vaasta) values ('781AMK', 'Chevrolet', 'Edgewire', 2010);
+insert into auto (registri_number, mark, omanik, vaasta) values ('931PJQ', 'Mercedes-Benz', 'Zoombeat', 1985);
+insert into auto (registri_number, mark, omanik, vaasta) values ('572IMA', 'Volkswagen', 'Voomm', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('979JQJ', 'Ford', 'Edgetag', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('340BZK', 'Buick', 'Mydeo', 1991);
+insert into auto (registri_number, mark, omanik, vaasta) values ('070MAC', 'Suzuki', 'Skidoo', 2009);
+insert into auto (registri_number, mark, omanik, vaasta) values ('404RJT', 'Mitsubishi', 'Jaloo', 1984);
+insert into auto (registri_number, mark, omanik, vaasta) values ('492IGM', 'Acura', 'Eimbee', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('203OEE', 'GMC', 'Twitterbridge', 2005);
+insert into auto (registri_number, mark, omanik, vaasta) values ('199EAR', 'BMW', 'Skivee', 2006);
+insert into auto (registri_number, mark, omanik, vaasta) values ('846ULE', 'Lincoln', 'Bluezoom', 2009);
+insert into auto (registri_number, mark, omanik, vaasta) values ('960ZYM', 'Kia', 'Gabspot', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('806PSU', 'GMC', 'Midel', 2008);
+insert into auto (registri_number, mark, omanik, vaasta) values ('786OSV', 'Toyota', 'Voolith', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('544FGJ', 'Chevrolet', 'Photofeed', 2010);
+insert into auto (registri_number, mark, omanik, vaasta) values ('018YHY', 'Mazda', 'Oyope', 1993);
+insert into auto (registri_number, mark, omanik, vaasta) values ('633DMP', 'Volkswagen', 'Tagtune', 1997);
+insert into auto (registri_number, mark, omanik, vaasta) values ('874PYB', 'Dodge', 'Chatterbridge', 1992);
+insert into auto (registri_number, mark, omanik, vaasta) values ('037WKD', 'Land Rover', 'Yakidoo', 1995);
+insert into auto (registri_number, mark, omanik, vaasta) values ('243RTL', 'Audi', 'Demimbu', 1992);
+insert into auto (registri_number, mark, omanik, vaasta) values ('973GFP', 'Buick', 'Layo', 1989);
+insert into auto (registri_number, mark, omanik, vaasta) values ('224MIS', 'Chevrolet', 'Jetpulse', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('044ISF', 'Chevrolet', 'Gabcube', 1997);
+insert into auto (registri_number, mark, omanik, vaasta) values ('823XOP', 'Ford', 'Innotype', 2012);
+insert into auto (registri_number, mark, omanik, vaasta) values ('823NGZ', 'Audi', 'Browsebug', 2000);
+insert into auto (registri_number, mark, omanik, vaasta) values ('433RHC', 'Toyota', 'Vinder', 2004);
+insert into auto (registri_number, mark, omanik, vaasta) values ('841ZLT', 'Lotus', 'Quaxo', 2007);
+insert into auto (registri_number, mark, omanik, vaasta) values ('443PKQ', 'Dodge', 'Camido', 2011);
+insert into auto (registri_number, mark, omanik, vaasta) values ('266FVQ', 'Volvo', 'Rhybox', 2003);
+insert into auto (registri_number, mark, omanik, vaasta) values ('354QWS', 'Toyota', 'Tavu', 1995);
+insert into auto (registri_number, mark, omanik, vaasta) values ('620RDV', 'BMW', 'Ozu', 2001);
+insert into auto (registri_number, mark, omanik, vaasta) values ('939FID', 'Honda', 'Tagpad', 2005);
+insert into auto (registri_number, mark, omanik, vaasta) values ('084QHJ', 'BMW', 'Innotype', 2008);
+
+select mark as '@mark',
+	registri_number,
+	mark,
+	vaasta,
+	omanik
+from auto for xml path('auto'), root('autod')
