de0690240e24ca0c6db9a6b8ddc3479f9b1d1099
Maksim Dotskin <124581027+DotskinMaksim@users.noreply.github.com>
2024-09-11 14:48:36 +0300

    Muudetud inserti kogus 50 peale ja lisatud kommentaarid

--- auto.sql
+++ auto.sql
@@ -1,7 +1,8 @@
+--andmebaasi loomine
 create database auto_tar;
 use auto_tar;
 
-
+--genereeritud https://www.mockaroo.com/ abil
 create table auto (
 	registri_number VARCHAR(50),
 	mark VARCHAR(50),
@@ -58,57 +59,10 @@ insert into auto (registri_number, mark, omanik, vaasta) values ('921KTK', 'Mazd
 insert into auto (registri_number, mark, omanik, vaasta) values ('150PAN', 'Scion', 'Wordware', 2009);
 insert into auto (registri_number, mark, omanik, vaasta) values ('840TZY', 'Dodge', 'Babbleset', 1999);
 insert into auto (registri_number, mark, omanik, vaasta) values ('337CEW', 'Ford', 'Voomm', 2000);
-insert into auto (registri_number, mark, omanik, vaasta) values ('240XUH', 'Maserati', 'Zazio', 2008);
-insert into auto (registri_number, mark, omanik, vaasta) values ('896EQE', 'BMW', 'Yata', 2001);
-insert into auto (registri_number, mark, omanik, vaasta) values ('611FNR', 'Lincoln', 'Katz', 2004);
-insert into auto (registri_number, mark, omanik, vaasta) values ('676YIT', 'Jeep', 'Brightdog', 2006);
-insert into auto (registri_number, mark, omanik, vaasta) values ('528IDP', 'BMW', 'Skippad', 2003);
-insert into auto (registri_number, mark, omanik, vaasta) values ('937CMD', 'GMC', 'Skyble', 1993);
-insert into auto (registri_number, mark, omanik, vaasta) values ('616QIU', 'Porsche', 'Katz', 2003);
-insert into auto (registri_number, mark, omanik, vaasta) values ('216BXG', 'Jeep', 'Jabbersphere', 2009);
-insert into auto (registri_number, mark, omanik, vaasta) values ('340BOU', 'Mitsubishi', 'Eabox', 1998);
-insert into auto (registri_number, mark, omanik, vaasta) values ('061HIX', 'Dodge', 'Meezzy', 2009);
-insert into auto (registri_number, mark, omanik, vaasta) values ('911SHV', 'Cadillac', 'Riffwire', 2008);
-insert into auto (registri_number, mark, omanik, vaasta) values ('909FNS', 'Toyota', 'Thoughtstorm', 2001);
-insert into auto (registri_number, mark, omanik, vaasta) values ('493XAU', 'Nissan', 'Ozu', 2010);
-insert into auto (registri_number, mark, omanik, vaasta) values ('935JRD', 'Kia', 'Yotz', 1999);
-insert into auto (registri_number, mark, omanik, vaasta) values ('778LKR', 'Dodge', 'Yabox', 2008);
-insert into auto (registri_number, mark, omanik, vaasta) values ('120KSB', 'Mercury', 'Zooveo', 1992);
-insert into auto (registri_number, mark, omanik, vaasta) values ('529NKO', 'Land Rover', 'Riffpath', 2005);
-insert into auto (registri_number, mark, omanik, vaasta) values ('781AMK', 'Chevrolet', 'Edgewire', 2010);
-insert into auto (registri_number, mark, omanik, vaasta) values ('931PJQ', 'Mercedes-Benz', 'Zoombeat', 1985);
-insert into auto (registri_number, mark, omanik, vaasta) values ('572IMA', 'Volkswagen', 'Voomm', 2000);
-insert into auto (registri_number, mark, omanik, vaasta) values ('979JQJ', 'Ford', 'Edgetag', 2000);
-insert into auto (registri_number, mark, omanik, vaasta) values ('340BZK', 'Buick', 'Mydeo', 1991);
-insert into auto (registri_number, mark, omanik, vaasta) values ('070MAC', 'Suzuki', 'Skidoo', 2009);
-insert into auto (registri_number, mark, omanik, vaasta) values ('404RJT', 'Mitsubishi', 'Jaloo', 1984);
-insert into auto (registri_number, mark, omanik, vaasta) values ('492IGM', 'Acura', 'Eimbee', 2001);
-insert into auto (registri_number, mark, omanik, vaasta) values ('203OEE', 'GMC', 'Twitterbridge', 2005);
-insert into auto (registri_number, mark, omanik, vaasta) values ('199EAR', 'BMW', 'Skivee', 2006);
-insert into auto (registri_number, mark, omanik, vaasta) values ('846ULE', 'Lincoln', 'Bluezoom', 2009);
-insert into auto (registri_number, mark, omanik, vaasta) values ('960ZYM', 'Kia', 'Gabspot', 2008);
-insert into auto (registri_number, mark, omanik, vaasta) values ('806PSU', 'GMC', 'Midel', 2008);
-insert into auto (registri_number, mark, omanik, vaasta) values ('786OSV', 'Toyota', 'Voolith', 2000);
-insert into auto (registri_number, mark, omanik, vaasta) values ('544FGJ', 'Chevrolet', 'Photofeed', 2010);
-insert into auto (registri_number, mark, omanik, vaasta) values ('018YHY', 'Mazda', 'Oyope', 1993);
-insert into auto (registri_number, mark, omanik, vaasta) values ('633DMP', 'Volkswagen', 'Tagtune', 1997);
-insert into auto (registri_number, mark, omanik, vaasta) values ('874PYB', 'Dodge', 'Chatterbridge', 1992);
-insert into auto (registri_number, mark, omanik, vaasta) values ('037WKD', 'Land Rover', 'Yakidoo', 1995);
-insert into auto (registri_number, mark, omanik, vaasta) values ('243RTL', 'Audi', 'Demimbu', 1992);
-insert into auto (registri_number, mark, omanik, vaasta) values ('973GFP', 'Buick', 'Layo', 1989);
-insert into auto (registri_number, mark, omanik, vaasta) values ('224MIS', 'Chevrolet', 'Jetpulse', 2001);
-insert into auto (registri_number, mark, omanik, vaasta) values ('044ISF', 'Chevrolet', 'Gabcube', 1997);
-insert into auto (registri_number, mark, omanik, vaasta) values ('823XOP', 'Ford', 'Innotype', 2012);
-insert into auto (registri_number, mark, omanik, vaasta) values ('823NGZ', 'Audi', 'Browsebug', 2000);
-insert into auto (registri_number, mark, omanik, vaasta) values ('433RHC', 'Toyota', 'Vinder', 2004);
-insert into auto (registri_number, mark, omanik, vaasta) values ('841ZLT', 'Lotus', 'Quaxo', 2007);
-insert into auto (registri_number, mark, omanik, vaasta) values ('443PKQ', 'Dodge', 'Camido', 2011);
-insert into auto (registri_number, mark, omanik, vaasta) values ('266FVQ', 'Volvo', 'Rhybox', 2003);
-insert into auto (registri_number, mark, omanik, vaasta) values ('354QWS', 'Toyota', 'Tavu', 1995);
-insert into auto (registri_number, mark, omanik, vaasta) values ('620RDV', 'BMW', 'Ozu', 2001);
-insert into auto (registri_number, mark, omanik, vaasta) values ('939FID', 'Honda', 'Tagpad', 2005);
-insert into auto (registri_number, mark, omanik, vaasta) values ('084QHJ', 'BMW', 'Innotype', 2008);
 
+
+--xml jada loomine
+-- mark as '@mark'- mark on auto atribuut <auto mark="Ford">
 select mark as '@mark',
 	registri_number,
 	mark,
